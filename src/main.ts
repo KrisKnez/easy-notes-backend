d27f55b6de677ad9d0cb6b4cc5f4930f446e52d2
Kristijan <kristijankej@gmail.com>
2023-10-12 22:27:41 +0200

    fix: remove unnecessary cors

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   app.enableCors({
     origin: process.env.CORS_ORIGINS || true,
     credentials: true,
-    preflightContinue: true,
-    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
   const config = new DocumentBuilder()
