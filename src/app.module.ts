719f5c4c37ba4ba5b2d3b0a25d582acc50e3a81f
Kristijan <kristijankej@gmail.com>
2024-06-02 14:32:32 +0200

    fix: vercel swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { PrismaService } from './shared/services/prisma/prisma.service';
 import { MeModule } from './modules/core/me/me.module';
 import { NotesModule } from './modules/feature/notes/notes.module';
 import { ContactsModule } from './modules/feature/contacts/contacts.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [AuthModule, MeModule, UsersModule, NotesModule, ContactsModule],
+  imports: [
+    AuthModule,
+    MeModule,
+    UsersModule,
+    NotesModule,
+    ContactsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'node_modules/swagger-ui-dist'),
+      serveRoot: '/api',
+    }),
+  ],
   controllers: [],
   providers: [PrismaService],
   exports: [PrismaService],
